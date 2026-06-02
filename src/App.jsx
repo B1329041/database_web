4cb1e54ad755f82a121366aea7b72aa20f9b5909
B1329041 <B1329041@cgu.edu.tw>
2026-06-02 19:35:37 +0800

    Remove ApiTester from GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
@@ -6,7 +7,6 @@ import Profile from './pages/Profile';
 import SetupProfile from './pages/SetupProfile';
 import PartyDetail from './pages/PartyDetail';
 import Admin from './pages/Admin';
-import ApiTester from './pages/ApiTester';
 import './App.css';
 
 function App() {
@@ -20,7 +20,6 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/party/:id" element={<PartyDetail />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/api-test" element={<ApiTester />} />
       </Routes>
     </Router>
   );
